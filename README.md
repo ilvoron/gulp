0e93366db35f3e62bf84c9a781ee4bfaab5d1595
coolpanda02 <46526864+coolpanda02@users.noreply.github.com>
2019-05-05 00:12:05 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,103 @@
+!ИНСТРУКЦИЯ ПО УСТАНОВКЕ GULP 4
+
+Для работы потребуется:
+	Node.js (чтобы устанавливать пакеты, в том числе и плагины для GULP)
+	Git (для работы некоторых плагинов)
+	ConEmu (консоль; для работы с GULP)
+
+Если вдруг GULP был до этого установлен (лучше ввести в любом случае)
+	Либо (предпочтительнее):
+		Для глобального удаления:
+			npm rm -g gulp
+			npm rm -g gulp-cli
+		Для локального удаления
+			npm rm --save-dev gulp
+			npm rm --save-dev gulp-cli
+			npm rm --save gulp
+			npm rm --save gulp-cli
+			npm rm --save-optional gulp
+			npm rm --save-optional gulp-cli
+		npm cache clean
+	Либо:
+		Для глобального удаления:
+			npm uninstall -g gulp
+			npm uninstall -g gulp-cli
+		Для локального удаления
+			npm uninstall --save-dev gulp
+			npm uninstall --save-dev gulp-cli
+			npm uninstall --save gulp
+			npm uninstall --save gulp-cli
+			npm uninstall --save-optional gulp
+			npm uninstall --save-optional gulp-cli
+		npm cache clean
+
+Устанавливаем GULP глобально
+npm install -g gulpjs/gulp-cli
+
+Создаем файл package.json
+npm init
+
+Устанавливаем GULP локально
+npm install --save-dev gulpjs/gulp
+
+Устанавливаем плагины для GULP
+	Устанавливаем по шаблону (локально):
+		npm install --save-dev <навзание_плагина>
+	Отдельно каждый:
+		Компиляция:
+			gulp-pug - компиляция PUG в HTML ( https://github.com/gulp-community/gulp-pug )
+			gulp-sass - компиляция SASS в CSS ( https://github.com/dlmanning/gulp-sass )
+		Транспиляция:
+			gulp-babel - конвертирует ES6/ES7 в ES5 ( https://github.com/babel/gulp-babel )
+				Для установки также нужно: @babel/core и @babel/preset-env
+		Конкатенация:
+			gulp-concat - конкатенация файлов ( https://github.com/wearefractal/gulp-concat )
+		Минификация:
+			На выбор (CSS минификатор):
+				[~] gulp-csso ( https://github.com/ben-eb/gulp-csso )
+				[~] gulp-clean-css ( https://github.com/scniro/gulp-clean-css )
+				[мой выбор] gulp-cssnano ( https://github.com/ben-eb/gulp-cssnano )
+			gulp-htmlmin - HTML минификатор ( https://github.com/jonschlinkert/gulp-htmlmin )
+			gulp-uglify - JavaScript компрессор ( https://github.com/terinjokes/gulp-uglify )
+			gulp-imagemin - минификация изображений формата PNG, JPEG, GIF и SVG (для него лучше gulp-svgmin) ( https://github.com/sindresorhus/gulp-imagemin )
+				Плагины:
+				imagemin-pngquant ( https://github.com/imagemin/imagemin-pngquant )
+				imagemin-mozjpeg ( https://github.com/imagemin/imagemin-mozjpeg )
+				imagemin-zopfli ( https://github.com/imagemin/imagemin-zopfli )
+			gulp-svgmin - минификация SVG ( https://github.com/ben-eb/gulp-svgmin )
+			gulp-strip-comments - удаляет комментарии из JSON, JavaScript, CSS, HTML и т.д. ( https://github.com/RnbWd/gulp-strip-comments )
+		Оптимизация:
+			gulp-uncss - анализирует HTML код и находит все неиспользуемые и продублированные стили, затем оптимизирует их ( https://github.com/ben-eb/gulp-uncss )
+			gulp-svgstore - Объединение SVG файлов в один <symbol> элемент ( https://github.com/w0rm/gulp-svgstore )
+		Линтинг:
+			[~] gulp-csslint - CSS линтер ( https://github.com/lazd/gulp-csslint )
+			[~] gulp-htmlhint - HTML валидатор ( https://github.com/bezoerb/gulp-htmlhint )
+			[~] gulp-jshint - поиск ошибок и потенциальных проблем в JavaScript ( https://github.com/spalger/gulp-jshint )
+			[~] gulp-jscs - JavaScript Code Style. проверка кода в соответствии с существующими стайлгайдами от jQuery, Яндекса, Google, Airbnb и других ( https://github.com/jscs-dev/gulp-jscs )
+			[~] gulp-plato - предоставляет аналитику по коду с разными метриками в виде красивых графиков ( https://github.com/sindresorhus/gulp-plato )
+			[~] gulp-complexity - проверка на качество кода основанная на алгоритмах Halstead и Cyclomatic ( https://github.com/alexeyraspopov/gulp-complexity )
+			[~] gulp-jscpd - для поиска дубликатов в коде ( https://github.com/yannickcr/gulp-jscpd )
+		Кэширование:
+			[~] gulp-changed - не надо больше тратить время на обработку неизмененных файлов ( https://github.com/sindresorhus/gulp-changed )
+			gulp-cached - простой кэщ файлов в памяти ( https://github.com/contra/gulp-cached )
+			[~] gulp-remember - кэширует файлы ( https://github.com/ahaurw01/gulp-remember )
+			[~] gulp-newer - позволяет работать только с измененным файлами, не трогая неизменненые ( https://github.com/tschaub/gulp-newer )
+		Логирование:
+			fancy-log - удобное логирование в консоли ( https://github.com/gulpjs/fancy-log )
+			chalk - ANSI цвета в консоли ( https://github.com/chalk/chalk )
+		Красивый код:
+			[~] gulp-csscomb - облагораживает структуру CSS (делает код красивым) ( https://github.com/koistya/gulp-csscomb )
+			[~] gulp-jsbeautifier - делает код JS красивым ( https://github.com/tarunc/gulp-jsbeautifier )
+			[~] gulp-html-beautify - делает структуру HTML красивой ( https://github.com/colynb/gulp-html-prettify )
+		Разное:
+			browser-sync - создает подключение, после чего производит автообновление страницы во всех браузерах на всех устройствах при изменениями не только клиентских или даже серверных файлов. А плюс ко всему синхронизирует позицию скроллинга и заполненные данные в формах.. ( https://browsersync.io )
+			bower - менеджер пакетов. Позволяет устанавливать в проект различные библиотеки (пакеты) одной командой (JQuery, Bootstrap, Font Awesome) ( https://bower.io )
+				('.bowerrc' => '{"directory": "app/libs/"}')
+			gulp-autoprefixer - автоматически расставляет префиксы к CSS свойствам, исходя из статистики caniuse ( https://github.com/sindresorhus/gulp-autoprefixer )
+			gulp-rename - переименовывание файлов ( https://github.com/hparra/gulp-rename )
+			del - удаление файлов и папок ( https://github.com/sindresorhus/del )
+			gulp-plumber - не останавливает выполнение кода из-за ошибок в .pipe() ( https://github.com/floatdrop/gulp-plumber )
+			[~] gulp-sourcemaps - создает sourcemap (карту кода) ( https://github.com/floridoo/gulp-sourcemaps )
+			[~] gulp-prompt - добавляет взаимодействие с GULP через консоль ( https://github.com/Freyskeyd/gulp-prompt#readme )
+	Установка всего сразу (те плагины, что отмечены [~] не устанавливаются):
+		npm install --save-dev gulp-pug gulp-sass gulp-babel @babel/core @babel/preset-env gulp-concat gulp-cssnano gulp-htmlmin gulp-uglify gulp-imagemin imagemin-pngquant imagemin-mozjpeg imagemin-zopfli gulp-svgmin gulp-strip-comments gulp-uncss gulp-svgstore gulp-cached fancy-log chalk browser-sync bower gulp-autoprefixer gulp-rename del gulp-plumber# gulp
---
+++ gulpfile.js
@@ -0,0 +1,393 @@
+'use strict';
+
+/*-----------*/
+/* Constants */
+/*-----------*/
+
+// Basic constants
+const { src, dest, series, watch } = require('gulp'); // This is Gulp
+
+// Compilation
+const pug  = require('gulp-pug');  // https://github.com/gulp-community/gulp-pug
+const sass = require('gulp-sass'); // https://github.com/dlmanning/gulp-sass
+
+// Transpilation
+const babel = require('gulp-babel'); // https://github.com/babel/gulp-babel
+
+// Concatenation
+const concat = require('gulp-concat'); // https://github.com/wearefractal/gulp-concat
+
+// Minification
+const cssnano  = require('gulp-cssnano');        // https://github.com/ben-eb/gulp-cssnano
+const htmlmin  = require('gulp-htmlmin');        // https://github.com/jonschlinkert/gulp-htmlmin
+const uglify   = require('gulp-uglify');         // https://github.com/terinjokes/gulp-uglify
+const imagemin = require('gulp-imagemin');       // https://github.com/sindresorhus/gulp-imagemin
+const pngquant = require('imagemin-pngquant');   // https://github.com/imagemin/imagemin-pngquant
+const mozjpeg  = require('imagemin-mozjpeg');    // https://github.com/imagemin/imagemin-mozjpeg
+const zopfli   = require('imagemin-zopfli');     // https://github.com/imagemin/imagemin-zopfli
+const svgmin   = require('gulp-svgmin');         // https://github.com/ben-eb/gulp-svgmin
+const strip    = require('gulp-strip-comments'); // https://github.com/RnbWd/gulp-strip-comments
+
+// Optimization
+const uncss    = require('gulp-uncss');    // https://github.com/ben-eb/gulp-uncss
+const svgstore = require('gulp-svgstore'); // https://github.com/w0rm/gulp-svgstore
+
+// Cashing
+const cache = require('gulp-cached'); // https://github.com/gulp-community/gulp-cached
+
+// Logging
+const log   = require('fancy-log');  // https://github.com/gulpjs/fancy-log
+const chalk = require('chalk');      // https://github.com/chalk/chalk
+
+// Miscellaneous
+const browserSync  = require('browser-sync').create(); // https://browsersync.io
+const autoprefixer = require('gulp-autoprefixer');     // https://github.com/sindresorhus/gulp-autoprefixer
+const rename       = require('gulp-rename');           // https://github.com/hparra/gulp-rename
+const del          = require('del');                   // https://github.com/sindresorhus/del
+const plumber      = require('gulp-plumber');          // https://github.com/floatdrop/gulp-plumber
+
+// Glob options
+let libsCss = []; // CSS libs
+let libsJs = []; // JS libs
+let toDeleteApp = ['app/**',
+	'!app',
+	'!app/index.pug',
+	'!app/fonts/**',
+	'!app/img/**',
+	'!app/js',
+	'!app/js/common.js',
+	'!app/libs/**',
+	'!app/pug/**',
+	'!app/sass/**']; // To clear "app" folder
+let toDeleteDest = ['dest']; // To clear "dest" folder
+let toDeleteDestOnlyImg = ['dest/img'];
+let toDeleteDestWithoutImg = toDeleteDestOnlyImg;
+	for (var ind = 0; ind < toDeleteDestWithoutImg.length; ind++) {
+		toDeleteDestWithoutImg[ind] = '!' + toDeleteDestWithoutImg[ind];
+	}
+	toDeleteDestWithoutImg = toDeleteDest.concat(toDeleteDestWithoutImg); // To clear "dest" folder without deleting images
+
+/*-----------*/
+/* Functions */
+/*-----------*/
+
+function pugCompilePart1() {
+	return src(['app/pug/**/*.pug', '!app/pug/_mixins.pug'])
+		.pipe(plumber())
+		.pipe(cache('pugCompiling'))
+		.pipe(pug())
+		.pipe(plumber.stop())
+		.pipe(dest('app/pages'));
+}
+
+function pugCompilePart2() {
+	return src('app/index.pug')
+		.pipe(plumber())
+		.pipe(cache('pugCompiling'))
+		.pipe(pug())
+		.pipe(plumber.stop())
+		.pipe(dest('app'));
+}
+
+function sassCompile() {
+	return src(['app/sass/**/*.sass', '!app/sass/_mixins.sass'])
+		.pipe(plumber())
+		.pipe(cache('sassCompiling'))
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer(['last 10 versions']))
+		.pipe(concat('style.min.css'))
+		.pipe(cssnano())
+		.pipe(plumber.stop())
+		.pipe(dest('app/css'))
+		.pipe(browserSync.reload({stream: true}));
+}
+
+function concatCss(done) {
+	if (libsCss.length > 0) {
+		return src(libsCss)
+			.pipe(plumber())
+			.pipe(concat('libs.min.css'))
+			.pipe(cssnano())
+			.pipe(plumber.stop())
+			.pipe(dest('app/css'))
+			.pipe(browserSync.reload({stream: true}));
+	} else {
+		done();
+	}
+}
+
+function concatJs(done) {
+	if (libsJs.length > 0) {
+		return src(libsJs)
+			.pipe(plumber())
+			.pipe(concat('libs.min.js'))
+			.pipe(uglify())
+			.pipe(plumber.stop())
+			.pipe(dest('app/js'));
+	} else {
+		done();
+	}
+}
+
+function createSprite() {
+	log(chalk.cyan('Minifying SVG images in one file...'));
+	del.sync('app/img/sprite.svg')
+	return src('app/img/**/*.svg', {
+			allowEmpty: true
+		})
+		.pipe(svgmin({
+			plugins: [
+				{removeStyleElement: true},
+				{removeTitle: true},
+				{removeDesc: true},
+				{removeUselessDefs: true},
+				{removeDimensions: true},
+				{removeRasterImages: true},
+				{collapseGroups: true},
+				{cleanupNumericValues: {
+						floatPrecision: 1
+					}},
+				{cleanupIDs: true},
+				{removeEmptyContainers: true},
+				{removeEmptyAttrs: true},
+				{cleanupAttrs: true}
+			]
+		}))
+		.pipe(rename({
+			prefix: 'icon-'
+		}))
+		.pipe(svgstore())
+		.pipe(rename({
+			basename: 'sprite'
+		}))
+		.pipe(dest('app/img'));
+}
+
+function liveReload() {
+	browserSync.init({
+		server: {
+			baseDir: 'app',
+			index: "index.html"
+		},
+		notify: false
+	})
+}
+
+/*-------------------------------------*/
+/* Cleaner. Reloader. Watcher. Builder */
+/*-------------------------------------*/
+
+function clearCache(done) {
+	cache.caches = {};
+	done();
+}
+
+function clearApp(done) {
+	del.sync(toDeleteApp);
+	log(chalk.cyan('Clearing "app" folder...'));
+	done();
+}
+
+function clearDest(done) {
+	del.sync(toDeleteDest);
+	log(chalk.cyan('Clearing "dest" folder...'));
+	done();
+}
+
+function clearDestWithoutImg(done) {
+	del.sync(toDeleteDestWithoutImg);
+	log(chalk.cyan('Clearing "dest" folder without deleting images...'));
+	done();
+}
+
+function clearDestOnlyImg(done) {
+	del.sync(toDeleteDestOnlyImg);
+	log(chalk.cyan('Clearing "dest" folder and only deleting images...'));
+	done();
+}
+
+function watcher() {
+	liveReload();
+	watch('app/sass/**/*.sass', sassCompile);
+	watch(['app/pug/**/*.pug', 'app/index.pug'], pugCompile);
+	watch(['app/pages/**/*.html', 'app/index.html']).on('change', browserSync.reload);
+	watch(['app/js/common.js', '!app/js/libs.min.js']).on('change', browserSync.reload);
+}
+
+function buildPart1() {
+	log(chalk.cyan('Recompiling PUG...'));
+	return src(['app/pug/**/*.pug', '!app/pug/_mixins.pug'])
+		.pipe(pug())
+		.pipe(htmlmin())
+		.pipe(dest('dest/pages'));
+}
+
+function buildPart2() {
+	log(chalk.cyan('Recompiling index.pug...'));
+	return src('app/index.pug')
+		.pipe(pug())
+		.pipe(htmlmin())
+		.pipe(dest('dest'));
+}
+
+function buildPart3() {
+	log(chalk.cyan('Recompiling SASS...'));
+	return src(['app/sass/**/*.sass', '!app/sass/_mixins.sass'])
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer(['last 10 versions']))
+		.pipe(concat('style.min.css'))
+		.pipe(uncss({
+			html: ['dest/pages/**/*.html', 'dest/index.html']
+		}))
+		.pipe(cssnano())
+		.pipe(dest('dest/css'));
+}
+
+function buildPart4() {
+	log(chalk.cyan('Working with common.js...'));
+	return src('app/js/common.js')
+		.pipe(babel({
+			presets: ['@babel/preset-env']
+		}))
+		.pipe(strip())
+		.pipe(dest('dest/js'));
+}
+
+function buildPart5(done) {
+	log(chalk.cyan('Reconcatenating CSS libs...'));
+	if (libsCss.length > 0) {
+		src(libsCss)
+			.pipe(concat('libs.min.css'))
+			.pipe(uncss({
+				html: ['dest/pages/**/*.html', 'dest/index.html']
+			}))
+			.pipe(cssnano())
+			.pipe(dest('dest/css'))
+	} else {
+		done();
+	}
+}
+
+function buildPart6(done) {
+	log(chalk.cyan('Reconcatenating JS libs...'));
+	if (libsJs.length > 0) {
+		src(libsJs)
+			.pipe(concat('libs.min.js'))
+			.pipe(uglify())
+			.pipe(dest('dest/js'));
+	} else {
+		done();
+	}
+}
+
+function buildPart7() {
+	log(chalk.cyan('Minifying images with .png, .gif, .jpg, .jpeg extensions...'));
+	return src('app/img/**/*.{png,gif,jpg,jpeg}', { //svg may be?
+			allowEmpty: true
+		})
+		.pipe(imagemin([
+			pngquant({
+				speed: 1,
+				quality: [0.9, 1]
+			}),
+			zopfli({
+				more: true,
+				iterations: 5 //50 slow, but more effective
+			}),
+			imagemin.gifsicle({
+				interlaced: true,
+				optimizationLevel: 3
+			}),
+			/* giflossy({
+				optimizationLevel: 3,
+				optimize: 'keep-empty',
+				lossy: 2
+			}), */
+			/* imagemin.svgo({
+				plugins: [{
+					removeViewBox: false
+				}]
+			}), */
+			imagemin.jpegtran({
+				progressive: true
+			}),
+			mozjpeg({
+				quality: 90
+			})
+		], {
+			verbose: true
+		}))
+		.pipe(dest('dest/img'));
+}
+
+function buildPart8() {
+	log(chalk.cyan('Minifying SVG images in one sprite...'));
+	return src('app/img/**/*.svg', {
+			allowEmpty: true
+		})
+		.pipe(svgmin({
+			plugins: [
+				{removeStyleElement: true},
+				{removeTitle: true},
+				{removeDesc: true},
+				{removeUselessDefs: true},
+				{removeDimensions: true},
+				{removeRasterImages: true},
+				{collapseGroups: true},
+				{cleanupNumericValues: {
+						floatPrecision: 1
+					}},
+				{cleanupIDs: true},
+				{removeEmptyContainers: true},
+				{removeEmptyAttrs: true},
+				{cleanupAttrs: true}
+			]
+		}))
+		.pipe(rename({
+			prefix: 'icon-'
+		}))
+		.pipe(svgstore())
+		.pipe(rename({
+			basename: 'sprite'
+		}))
+		.pipe(dest('dest/img'));
+}
+
+function buildPart9() {
+	log(chalk.cyan('Copying other images...'));
+	return src(['app/img/**/*.*', '!app/img/**/*.{png,gif,jpg,jpeg,svg}'], {
+			allowEmpty: true
+		})
+		.pipe(dest('dest/img'));
+}
+
+/*--------------------*/
+/* Register functions */
+/*--------------------*/
+
+// Templates
+let pugCompile = series(pugCompilePart1, pugCompilePart2);
+let clearAll   = series(clearCache, clearApp, clearDest);
+let build      = series(buildPart1, buildPart2, buildPart3, buildPart4, buildPart5, buildPart6);
+
+exports.pugCompile   = pugCompile;   // Compile .pug files 
+exports.sassCompile  = sassCompile;  // Compile .sass files, concatenate and minify
+exports.concatCss    = concatCss;    // Concatenate css libs and minify
+exports.concatJs     = concatJs;     // Concatenate js libs and minify
+exports.createSprite = createSprite; // Create SVG sprite
+exports.liveReload   = liveReload;   // Initializing browser synchronization
+
+exports.clearCache          = clearCache;          // Clears the cache
+exports.clearApp            = clearApp;            // Deleting unnecessary (or compiled) files and folders in "app" folder
+exports.clearDest           = clearDest;           // Delete "dest" folder
+exports.clearDestWithoutImg = clearDestWithoutImg; // Delete "dest" folder without deleting images
+exports.clearDestOnlyImg    = clearDestOnlyImg;    // In "dest" folder delete only images
+exports.clearAll            = clearAll;            // Deleting unnecessary (or compiled) files and folders in "app" folder. Delete "dest" folder. Clears the cache
+exports.watcher             = watcher;             // Initializing watcher
+
+exports.build               = series(clearDest, build, buildPart7, buildPart8, buildPart9); // Build project and compress images
+exports.buildWithoutImg     = series(clearDestWithoutImg, build);                           // Build project without compress images
+exports.buildOnlyImg        = series(clearDestOnlyImg, buildPart7, buildPart8, buildPart9); // Only compress images
+
+// As default just development begins
+exports.default = series(clearAll, sassCompile, pugCompile, concatCss, concatJs, createSprite, watcher);
